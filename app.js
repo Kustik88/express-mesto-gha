ae6245b2e8e0cdeb7bcbef25a19f4ecff53f6614
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-05-25 01:37:58 +0600

    bugаix: исправить замечания автотестов

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const routerCards = require('./routes/cards')
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 const app = express()
 app.use(bodyParser.json())
-// app.use((req, res, next) => {
-//   if(req.params._id)
-//   res.status(404).send({ message: 'Запрашиваемая страница не найдена' })
 
-//   next()
-// })
 app.use((req, res, next) => {
   req.user = {
     _id: '646cfe1a939835021a970a0c',
@@ -23,6 +18,10 @@ app.use((req, res, next) => {
 
 app.use(routerUsers)
 app.use(routerCards)
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемая страница не найдена' })
+})
 
 app.listen(3000, () => {
   console.log('Server is running')
